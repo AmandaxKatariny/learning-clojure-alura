f159eb77bfbc539362c462d5f92ccfa17b0e4224
Amanda Katariny <amandakata@outlook.com>
2021-11-07 10:33:51 -0300

    feat(functions and parameters):primeiros conceitos de funções e parametros

---
+++ functions-and-parameters.clj
@@ -0,0 +1,22 @@
+(defn imprime-mensagem []
+  (println "-----------------------")
+  (println "Bem vindo(a) ao estoque"))
+
+(imprime-mensagem)
+--------------------------------------
+
+(defn valor-descontado 
+  "Retorna o valor descontado que é 90% do valor bruto."
+  [valor-bruto]
+  (* valor-bruto 0.9))
+
+(valor-descontado 100)
+--------------------------------------
+
+(defn valor-descontado 
+  "Retorna o valor com descontado de 10%."
+  [valor-bruto]
+  (* valor-bruto (- 1 0.10)))
+
+(valor-descontado 100)
+
