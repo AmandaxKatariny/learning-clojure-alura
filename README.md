49003f2081e68d700b4a04aa25f59a0829cd82df
Amanda Katariny <amandakata@outlook.com>
2021-11-12 19:00:41 -0300

    feat(aula 3): finalizada

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ Primeiros passos no aprendizado de Clojure - Curso da Alura
 ## Comandos
 Após a instalação basta abrir o terminal e usar o comando `clj`
 
+## Sobre
+
+O Cursive possui dezenas de atalhos que nos ajudam no dia a dia. O guia de usuário deles possui diversas páginas que sugiro ler de vez em quando. Não precisa ler tudo de uma vez só. Uma vez por semana olha uma página nova de atalhos para você tentar incorporar ao seu dia a dia. Lembre-se que no dia a dia gastamos mais tempo pensando e elaborando o que fazer do que digitando, não estamos numa competição de digitação, mas provavelmente queremos produzir código bom que seja de manutenção boa a médio prazo. https://cursive-ide.com/userguide
+
 ## Aulas
 Conteudo programático das Aulas - Alura
 ### Aula 01
@@ -39,6 +43,20 @@ O que aprendemos nesta aula:
 - Utilizar o class para descobrir o tipo da variável;
 - Trabalhar com condicionais if;
 - Que o nil(Nulo) é considerado false dentro do if.
+
+
+## Aula 03 
+
+O que aprendemos nesta aula:
+
+- Utilizar o plugin Cursive;
+- O que é o namespace;
+- Atalhos do Intellij;
+- Utilizar o ; para comentar a linha;
+- O que são predicates;
+- Fazer uma função chamar a outra;
+- Criar uma função anônima;
+- Utilizar % para fazer um função lambda.
 
 ## Conceitos 
 
--- src/learning_clojure_alura/aula3.clj
+++ src/learning_clojure_alura/aula3.clj
@@ -37,3 +37,62 @@
 (println (aplica-desconto? 100))
 (valor-descontado 1000)
 (valor-descontado 100)
+
+
+(defn mais-caro-que-100?
+  "Verifica se valor é maior que 100"
+  [valor-bruto]
+  (> valor-bruto 100))
+
+
+(defn valor-descontado
+  "Retorna o valor com descontado de 10%."
+  [aplica? valor-bruto]
+  (if (aplica? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (println "Calculando desconto de" desconto)
+      (- valor-bruto desconto))
+    valor-bruto))
+
+
+(println "Função como parâmetro")
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100? 100))
+
+; Higher order Functions
+; Imutabilidade
+
+(println "Função sem nome, anonima")
+(println (valor-descontado
+           (fn [valor-bruto]
+              (> valor-bruto 100))
+           1000))
+
+(println (valor-descontado
+           (fn [valor-bruto]
+              (> valor-bruto 100))
+           100))
+
+(println (valor-descontado (fn [v] (> v 100)) 100))
+(println (valor-descontado (fn [v] (> v 100)) 1000))
+
+(println (valor-descontado #(> %1 100) 100))
+(println (valor-descontado #(> %1 100) 1000))
+
+(println (valor-descontado #(> % 100) 100))
+(println (valor-descontado #(> % 100) 1000))
+
+
+(def mais-caro-que-100? (fn [valor-bruto] (> valor-bruto 100)))
+(def mais-caro-que-100? #(> % 100))
+
+(println (valor-descontado
+           (fn [valor-bruto]
+             (> valor-bruto 100))
+           1000))
+
+(println (valor-descontado
+           (fn [valor-bruto]
+             (> valor-bruto 100))
+           100))
